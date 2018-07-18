5361adf39e714e4ce9ff6986b82507a7b0dab291
fer <fernandoza@gmail.com>
2018-07-18 12:31:26 -0500

    feat: add solution to the exercise

--- src/index.js
+++ src/index.js
@@ -1,79 +1,88 @@
 "use strict";
 
-// const name = require('./req');
-// console.log(name);
+import {sayHello} from './say-hello.js';
 
+sayHello('Fer');
+sayHello('Ana');
+sayHello('Luis');
 
+sayHello(1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ================= REQUIRE AS A SINGLE VALUE
-
-    // const message = require('./require-example');
-    // console.log(message);
-
-
-// ================= REQUIRE AS A SINGLE OBJECT
-
-    // const { sayName, additionalMessage } = require('./require-example'); // using destructuring
-    //
-    // console.log(additionalMessage);
-    // sayName('Justin');
-
-// ================= IMPORT WITH SEPARATE EXPORTS
 //
-//     import  {importMessage as message, anotherMessage, sayLastName}  from './import-example.js';
+// // const name = require('./req');
+// // console.log(name);
 //
-//     console.log(message);
-//     console.log(anotherMessage);
-//     sayLastName('Reich');
-
-
-// ================= IMPORT WITH A SINGLE EXPORT
-
-    // import { importMessage, anotherMessage, sayLastName } from './import-example.js';
-    // console.log(importMessage);
-    // console.log(anotherMessage);
-    // sayLastName('Reich');
-
-
-// ================= IMPORT SINGLE OBJECT WITH EXPORT DEFAULT
-
-    // import importExample from './import-example.js';
-    // console.log(importExample.importMessage);
-    // console.log(importExample.anotherMessage);
-
-
-// ================= API EXAMPLE
-
-
-    import keys from './import-example.js';
-
-    const token = keys.GITHUB_API_TOKEN;
-
-    const getEvents = (username) => {
-        const options = {
-            headers: {
-                Authorization: `token ${token}`,
-            }
-        };
-        const url = `https://api.github.com/users/${username}/events/public`;
-        return fetch(url);
-    };
-
-    getEvents('jreich5')
-        .then(response => response.json())
-        .then(data => {console.log(data)})
-        .catch(e => console.log(e));
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // ================= REQUIRE AS A SINGLE VALUE
+//
+//     // const message = require('./require-example');
+//     // console.log(message);
+//
+//
+// // ================= REQUIRE AS A SINGLE OBJECT
+//
+//     // const { sayName, additionalMessage } = require('./require-example'); // using destructuring
+//     //
+//     // console.log(additionalMessage);
+//     // sayName('Justin');
+//
+// // ================= IMPORT WITH SEPARATE EXPORTS
+// //
+// //     import  {importMessage as message, anotherMessage, sayLastName}  from './import-example.js';
+// //
+// //     console.log(message);
+// //     console.log(anotherMessage);
+// //     sayLastName('Reich');
+//
+//
+// // ================= IMPORT WITH A SINGLE EXPORT
+//
+//     // import { importMessage, anotherMessage, sayLastName } from './import-example.js';
+//     // console.log(importMessage);
+//     // console.log(anotherMessage);
+//     // sayLastName('Reich');
+//
+//
+// // ================= IMPORT SINGLE OBJECT WITH EXPORT DEFAULT
+//
+//     // import importExample from './import-example.js';
+//     // console.log(importExample.importMessage);
+//     // console.log(importExample.anotherMessage);
+//
+//
+// // ================= API EXAMPLE
+//
+//
+//     import keys from './import-example.js';
+//
+//     const token = keys.GITHUB_API_TOKEN;
+//
+//     const getEvents = (username) => {
+//         const options = {
+//             headers: {
+//                 Authorization: `token ${token}`,
+//             }
+//         };
+//         const url = `https://api.github.com/users/${username}/events/public`;
+//         return fetch(url);
+//     };
+//
+//     getEvents('jreich5')
+//         .then(response => response.json())
+//         .then(data => {console.log(data)})
+//         .catch(e => console.log(e));
