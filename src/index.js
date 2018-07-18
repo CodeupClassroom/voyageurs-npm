cf56422e92e9c20caaea13a261390c08bbf8cc19
Justin Reich <justinreich.dev@gmail.com>
2018-07-18 08:56:11 -0500

    Add examples

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-const $ = require('jquery');
+// ================= REQUIRE AS A SINGLE VALUE
 
-$('body').css('background-color', 'cyan');
+    // const message = require('./require-example');
+    // console.log(message);
 
-let sayHello = name => console.log(`Hello ${name}`);
 
-sayHello('Fer');
-sayHello('Ana');
+// ================= REQUIRE AS A SINGLE OBJECT
+
+    // const { sayName, additionalMessage } = require('./require-example'); // using destructuring
+
+
+
+// ================= IMPORT WITH SEPARATE EXPORTS
+
+    // import { importMessage, anotherMessage, sayLastName } from './import-example.js';
+    // console.log(importMessage);
+    // console.log(anotherMessage);
+    // sayLastName('Reich');
+
+
+// ================= IMPORT WITH A SINGLE EXPORT
+
+    // import { importMessage, anotherMessage, sayLastName } from './import-example.js';
+    // console.log(importMessage);
+    // console.log(anotherMessage);
+    // sayLastName('Reich');
+
+
+// ================= IMPORT SINGLE OBJECT WITH EXPORT DEFAULT
+
+    // import importExample from './import-example.js';
+    // console.log(importExample.importMessage);
+    // console.log(importExample.anotherMessage);
+
+
+// ================= API EXAMPLE
+
+    // import keys from './credentials';
+    // const token = keys.GITHUB_API_TOKEN;
+    //
+    // const getEvents = (username) => {
+    //     const options = {
+    //         headers: {
+    //             Authorization: `token ${token}`,
+    //         }
+    //     };
+    //     const url = `https://api.github.com/users/${username}/events/public`;
+    //     return fetch(url);
+    // };
+    //
+    // getEvents('jreich5')
+    //     .then(response => response.json())
+    //     .then(data => {console.log(data)})
+    //     .catch(e => console.log(e));
---
+++ src/require-example.js
@@ -0,0 +1,17 @@
+// REQUIRE AS A SINGLE VALUE
+
+
+    // module.exports = "Hello from require example!";
+
+
+// REQUIRE AS A SINGLE OBJECT
+
+    // const additionalMessage = "Another hello!";
+    //
+    // const sayName = name => {
+    //   console.log(`My name is: ${name}`);
+    // };
+    //
+    //
+    // module.exports = { additionalMessage, sayName };
+
